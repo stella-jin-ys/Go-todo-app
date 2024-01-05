6d7542d202fda1c946f421c88f38d1b0c48aba42
Stella <stella.jin123@gmail.com>
2024-01-05 13:23:11 +0100

    update request

--- server/main.go
+++ server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import ("fmt" 
+"github.com/gofiber/fiber/v2"
+"log"
+)
+
+type Todo struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Done bool `json:"done"`
+	Body int `json:"body`
+}
 
 func main(){
 	fmt.Print("Hello world")
+
+	app:= fiber.New()
+	todos := []Todo{}
+
+	app.Get("/healthcare", func(c *fiber.Ctx) error{
+		return c.SendString("OK")
+	})
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo) err!= nil{
+			return err
+		}
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
